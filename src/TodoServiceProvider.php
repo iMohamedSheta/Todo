41d3c5c25833c3800b5de1b67723c01c02c6abca
iMohamedSheta <mohamed15.sheta15@gmail.com>
2025-02-19 21:17:53 +0200

    fix

--- src/TodoServiceProvider.php
+++ src/TodoServiceProvider.php
@@ -4,15 +4,9 @@ namespace IMohamedSheta\Todo;
 
 use Illuminate\Support\ServiceProvider;
 use IMohamedSheta\Todo\Commands\TodoScanCommand;
-use IMohamedSheta\Todo\Services\FileCollectorService;
 
 class TodoServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        $this->app->singleton(FileCollectorService::class, fn($app): \IMohamedSheta\Todo\Services\FileCollectorService => new FileCollectorService);
-    }
-
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
